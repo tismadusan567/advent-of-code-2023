28794d3c430b4deea3e3bff1ef2dd143ad8db996
Dusan Tisma <tismadusan567@gmail.com>
2023-12-05 04:33:09 +0100

    trebuchet

---
+++ trebuchet/trebuchet.hs
@@ -0,0 +1,14 @@
+import Data.Char (isDigit, digitToInt)
+import Data.Foldable ( find )
+import qualified Data.Maybe
+
+sumOfCalibration :: [String] -> Int
+sumOfCalibration strings = sum $ map parseString strings
+    where 
+        parseString str = firstNum str * 10 + firstNum (reverse str)
+        firstNum str = digitToInt $ Data.Maybe.fromMaybe '0' (find isDigit str)
+
+main :: IO ()
+main = do
+    s <- readFile "trebuchet\\input.txt"
+    print $ sumOfCalibration (lines s)
