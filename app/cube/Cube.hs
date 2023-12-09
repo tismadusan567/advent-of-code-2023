2a1fdd48eb2ed868b3236b91df26f0e7bb23dfac
Dusan Tisma <tismadusan567@gmail.com>
2023-12-09 17:56:24 +0100

    cube 2

--- app/cube/Cube.hs
+++ app/cube/Cube.hs
@@ -1,15 +1,37 @@
 module Cube where
 
 import Data.List.Split
-import Data.Map.Strict (fromList, (!), Map, keys)
+import Data.Map.Strict (fromList, (!), Map, keys, insertWith)
 import Data.Maybe
 
+-- part one
+
 sumOfGameIds :: [String] -> Int
-sumOfGameIds strings = sum . catMaybes $ map validGame strings
+sumOfGameIds games = sum . catMaybes $ map validGame games
 
 validGame :: String -> Maybe Int
 validGame str = if validMoves moves then Just gameId else Nothing
     where (gameId, moves) = parseGame str
+
+validMoves :: [Map String Int] -> Bool
+validMoves moves = foldl validMove True moves
+    where 
+        validMove acc move = if acc then foldl (validColor move) True (keys move) else False
+        validColor move acc color = if acc then move ! color <= colorMap ! color else False
+        colorMap = fromList [("blue", 14), ("green", 13), ("red", 12)]
+
+-- part two
+
+sumOfPowers :: [String] -> Int
+sumOfPowers games = sum $ map gamePower games
+
+gamePower :: String -> Int
+gamePower game = foldl (*) 1 $ foldl computeMove (fromList [("blue", 0), ("red", 0), ("green", 0)]) moves
+    where 
+        (_, moves) = parseGame game
+        computeMove acc move = foldl (\acc2 key -> insertWith max key (move ! key) acc2) acc (keys move)
+
+-- helpers
 
 parseGame :: String -> (Int, [Map String Int])
 parseGame str = (gameId, parseMoves moves)
@@ -25,14 +47,8 @@ parseMove move = fromList $ map parseEntry (splitOn "," move)
     where 
         parseEntry entry = let [num, color] = words entry in (color, read num)
 
-validMoves :: [Map String Int] -> Bool
-validMoves moves = foldl validMove True moves
-    where 
-        validMove acc move = if acc then foldl (validColor move) True (keys move) else False
-        validColor move acc color = if acc then move ! color <= colorMap ! color else False
-        colorMap = fromList [("blue", 14), ("green", 13), ("red", 12)]
-
 cubeMain :: IO ()
 cubeMain = do
     s <- readFile "app\\cube\\input.txt"
     print $ sumOfGameIds (lines s)
+    print $ sumOfPowers (lines s)
