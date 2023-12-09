551634da8573a3baaa5e560b5e175b0ceaf40d1c
Dusan Tisma <tismadusan567@gmail.com>
2023-12-09 17:11:37 +0100

    start refactor

--- app/cube/Cube.hs
+++ app/cube/Cube.hs
@@ -1,20 +1,32 @@
 module Cube where
 
 import Data.List.Split
-import Data.Map.Strict (fromList, (!))
+import Data.Map.Strict (fromList, (!), Map)
 import Data.Maybe
 
 sumOfGameIds :: [String] -> Int
-sumOfGameIds strings = sum . catMaybes $ map parseGame strings
+sumOfGameIds strings = sum . catMaybes $ map validGame strings
 
-parseGame :: String -> Maybe Int
-parseGame str = if valid cubes then Just gameId else Nothing
+validGame :: String -> Maybe Int
+validGame str = if validMoves moves then Just gameId else Nothing
+    where (gameId, moves) = parseGame str
+
+parseGame :: String -> (Int, String)
+parseGame str = (gameId, moves)
     where 
-        [game, cubes]= splitOn ":" str
+        [game, moves]= splitOn ":" str
         gameId = read $ (words $ game) !! 1
 
-valid :: String -> Bool
-valid str = foldl validSet True (splitOn ";" str)
+parseMoves :: String -> [Map String Int]
+parseMoves moves = map parseMove (splitOn ";" moves)
+
+parseMove :: String -> Map String Int
+parseMove move = fromList $ map parseEntry (splitOn "," move)
+    where 
+        parseEntry entry = let [num, color] = words entry in (color, read num)
+
+validMoves :: String -> Bool
+validMoves str = foldl validSet True (splitOn ";" str)
     where 
         validSet acc set = if acc then foldl validEntry True (splitOn "," set) else False
         validEntry acc entry = let [num, color] = words entry in if acc then (read num) <= (colorMap ! color) else False
